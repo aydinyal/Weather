e5c8d62605041dbf751e56ba04a72e23cb74cb02
Ziad <ziad.safari1@gmail.com>
2021-06-08 22:20:23 -0400

    ziad commit

---
+++ src/components/card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './card.css';
+
+const Card = () => (
+    <div className="card">
+      <p>This is some content in sticky footer fck you</p>
+    </div>
+  );
+  
+  export default Card;
